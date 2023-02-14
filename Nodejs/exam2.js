b76043e1c4ddd398e3043508a331110771e7c0bb
anju12042001 <83828838+anju12042001@users.noreply.github.com>
2023-02-14 21:23:20 +0530

    commit

--- Nodejs/exam2.js
+++ Nodejs/exam2.js
@@ -1,12 +1,10 @@
-<html>
-  <body>
 var a = "Abc";
             eg = document.getElementById("test");
             eg.innerHTML = a;
 
             //Difference between var and let
-            let p = 5;
-            var q = 6;
+            let p = 10;
+            var q = 11;
             {
                 let p = 3;
                 var q = 4;
@@ -21,5 +19,3 @@ var a = "Abc";
               console.log("true");
             else if(x==null)
               console.log("false");
-    </body>
-</html>
