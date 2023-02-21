6edfef2d80abe6a80bec35cc0a23acb633ecf5ad
anju12042001 <83828838+anju12042001@users.noreply.github.com>
2023-02-21 20:55:45 +0530

    commit

---
+++ Nodejs/basic_express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("route at root");
+
+});
+ app.get("/hello", (req, res) => {
+    res.send("route at hello");
+ });
+  app.listen(3000, () => {
+    console.log("Listeing on 3000");
+  });
