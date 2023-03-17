9814cc82975d31b148f921df63e5bd74389c548f
anju12042001 <83828838+anju12042001@users.noreply.github.com>
2023-03-17 16:11:10 +0530

    commit

---
+++ VUE.JS/app.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Helloworld from './components/Helloworld.vue'
+import AboutYou from './components/AboutYou.vue'
+
+</script>
+
+<template>
+<header>
+<div class="wrapper">
+<br>
+<nav>
+<RouterLink to="/">Home</RouterLink>
+<RouterLink to="/about">About</RouterLink>
+<RouterLink to="/test">Test</RouterLink>
+<RouterLink to="/login">Login</RouterLink>
+<RouterLink to="/aboutyou">AboutYou</RouterLink>
+<RouterLink to="/products">Products</RouterLink>
+</nav>
+<br>
+</div>
+</header>
+
+<RouterView />
+</template>
+
+<style scoped>
+header {
+    
+}
---
+++ VUE.JS/hello-world/template.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+<div class="container">
+    <h1>hello everyone {{ msg }}</h1>
+    <h2>
+        You've successfully login to your project
+        <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a>+
+        <a href="https://vuejs.org/" target="_blacnk" rel="noopener">Vue 3</a>.
+    </h2>
+</div>
+</template>
+
+<style>
+
+.container{
+    text-align: center;
+}
